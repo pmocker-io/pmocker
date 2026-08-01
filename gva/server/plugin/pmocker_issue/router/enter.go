ee262cddf9ec6ff66346cdf4ee0b848587358cdc
pmocker-io <chenkaidf@qq.com>
2026-08-01 11:07:34 +0800

    fix(issue): 补全 issue 模块 enter.go 防止编译失败
    router/enter.go 和 service/enter.go 此前内容被截断导致 EOF 语法错误，
    按其他模块的统一格式补全 RouterGroup、ApiGroupApp、ServiceGroupApp 定义。

--- gva/server/plugin/pmocker_issue/router/enter.go
+++ gva/server/plugin/pmocker_issue/router/enter.go
@@ -1,1 +1,12 @@
-package
+package issue
+
+import api "github.com/flipped-aurora/gin-vue-admin/server/plugin/pmocker_issue/api"
+
+var issueImpl = &Issue{}
+
+type RouterGroup struct {
+	Issue *Issue
+}
+
+var RouterGroupApp = &RouterGroup{Issue: issueImpl}
+var ApiGroupApp = api.ApiGroupApp
